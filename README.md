4406361073e0e1f95e9665aaba7797c6de902db7
HongKun Tian <hongkun.tian@hotmail.com>
2019-01-19 20:38:32 -0500

    asdf

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sureview
+# sureviews
--- app/analyser.py
+++ app/analyser.py
@@ -1,12 +1,16 @@
 from google.cloud import language
 from google.cloud.language import enums, types
 from google.cloud import storage
+import os
+import numpy as np
+from scipy.spatial.distance import cosine
+from math import sqrt
 
 # Instantiates a client
 gcl = language.LanguageServiceClient()
 gcs = storage.Client()
 
-
+wordvectors = {}
 
 def analyse( text ):
     document = types.Document( #pylint: disable=E1101
@@ -22,4 +26,18 @@ def analyse( text ):
         key = lambda e: str.lower(e['name'])
     )
 
+if os.path.isfile('./wordvec.txt'):
+    with open('./wordvec.txt') as f:
+        raw = f.readlines()
+else:
+    bucket = gcs.get_bucket('third-wharf-229116.appspot.com')
+    blob = storage.Blob('wordvec.txt', bucket)
+    raw = blob.download_as_string()
+
+for line in raw:
+    l = line.split()
+    wordvectors[l[0]] = np.array([float(x) for x in l[1:]])
+
+def related(a, b):
+    return cosine(wordvectors[a],wordvectors[b]) < 0.36
 
